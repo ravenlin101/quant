d015a81fa895c4c1d7b20f93b112aea13c427067
ravenlin101 <ravenlin101@gmail.com>
2018-07-18 18:01:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,57 @@
+from futuquant import *
+import pandas as pd
+import numpy as np
 import mysql.connector
-import datetime
-import sys
+import sqlite3
+import matplotlib.pyplot as plt
+import pylab as pl
 
-conn = mysql.connector.connect(
+def get_stockID(stockCode):
+    quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
+    lines = quote_ctx.get_stock_basicinfo(Market.HK, SecurityType.STOCK)
+    df = lines[1]
+    stockId = (df.loc[df['code'] == stockCode])['stock_id'].to_string(index = False) 
+    quote_ctx.close()
+    return stockId
+
+cMysql = mysql.connector.connect(
          user='root',
          password='root',
-         host='10.231.30.60',
+         host='127.0.0.1',
          database='scrapy')
-cursor = conn.cursor()
+cursorMysql = cMysql.cursor()
 
 sql = "SELECT * FROM aastock;"
 
-cursor.execute(sql)
+cursorMysql.execute(sql)
 
-results = cursor.fetchall()
-print(results[0][0])
+results = cursorMysql.fetchall()
+
+stockID = get_stockID('HK.'+results[0][2][0:5])
+
+connSqlite = sqlite3.connect("D:\\ft_hist_data\\hist_hk\hknor_kl_day_0.db")
+
+cursorSqlite = connSqlite.cursor()
+
+sql2 = "SELECT date(time_desc) FROM KLData where stock_id = %s limit 10;" % stockID
+sql3 = "SELECT close_price FROM KLData where stock_id = %s limit 10;" % stockID
+
+cursorSqlite.execute(sql2)
+
+x = cursorSqlite.fetchall()
+
+tDate = []
+cPrice = []
+
+for a in x:
+    tDate.append(a[0])
+
+
+cursorSqlite.execute(sql3)
+y = cursorSqlite.fetchall()
+
+for b in y:
+    cPrice.append(b[0])
+
+print(tDate)
+print(cPrice)
